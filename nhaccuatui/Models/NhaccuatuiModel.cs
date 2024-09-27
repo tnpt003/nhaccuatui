5e8db9cb412b6e3887e9019506f730464d289543
tnpt003 <tnpt003@gmail.com>
2024-09-27 15:50:05 +0700

    thêm extension, merge footer - menu - banner, kết nối database

---
+++ nhaccuatui/Models/NhaccuatuiModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace nhaccuatui.Models
+{
+    public class NhaccuatuiModel
+    {
+        private String connectionString = "workstation id=nhaccuatui.mssql.somee.com;packet size=4096;user id=tnpt003_SQLLogin_1;pwd=aj3vd8m6jx;data source=nhaccuatui.mssql.somee.com;persist security info=False;initial catalog=nhaccuatui;TrustServerCertificate=True";
+        
+    }
+
+}
