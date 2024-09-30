04c8ba3e7b32710005b6a7e50d59c1f7eeff35c0
luchithanhdeptrai123 <144644481+luchithanhdeptrai123@users.noreply.github.com>
2024-09-30 11:46:41 +0700

    Add files via upload

---
+++ nhaccuatui/dangkydangnhap/login-register.js
@@ -0,0 +1,19 @@
+﻿document.getElementById('registerBtn').addEventListener('click', function () {
+    document.getElementById('registerForm').classList.add('active');
+    document.getElementById('loginForm').classList.remove('active');
+});
+
+document.getElementById('loginBtn').addEventListener('click', function () {
+    document.getElementById('loginForm').classList.add('active');
+    document.getElementById('registerForm').classList.remove('active');
+});
+
+document.getElementById('showRegister').addEventListener('click', function () {
+    document.getElementById('registerForm').classList.add('active');
+    document.getElementById('loginForm').classList.remove('active');
+});
+
+document.getElementById('showLogin').addEventListener('click', function () {
+    document.getElementById('loginForm').classList.add('active');
+    document.getElementById('registerForm').classList.remove('active');
+});
