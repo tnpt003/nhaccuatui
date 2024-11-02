660ff4220d7ea593f2e2bea71d0e93375869da7d
tnpt003 <tnpt003@gmail.com>
2024-11-02 18:36:53 +0700

    thêm xóa cho ca sĩ

---
+++ nhaccuatui/Controllers/ArtistController.cs
@@ -0,0 +1,32 @@
+﻿using nhaccuatui.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace nhaccuatui.Controllers
+{
+    public class ArtistController : Controller
+    {
+        private readonly NhaccuatuiModel db = new NhaccuatuiModel();
+        // GET: Artist
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddArtist(string name, string country, string bio)
+        {
+            db.get($"INSERT INTO Artists (Name, Bio, Country) VALUES (N'{name}', N'{bio}', N'{country}')");
+            return RedirectToAction("Index", "Admin");
+        }
+
+        public ActionResult DeleteArtist(int id)
+        {
+            db.get($"DELETE FROM Artists WHERE ArtistID = {id}");
+            return RedirectToAction("Index", "Admin");
+        }
+    }
+}
