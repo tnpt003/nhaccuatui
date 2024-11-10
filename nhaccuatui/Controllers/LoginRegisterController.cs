60d9a419c981102d4510789e63fc55d34d7ab919
tnpt003 <tnpt003@gmail.com>
2024-11-10 23:28:55 +0700

    chuc nang dang nhap

--- nhaccuatui/Controllers/LoginRegisterController.cs
+++ nhaccuatui/Controllers/LoginRegisterController.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.DynamicData;
 using System.Web.Mvc;
+using nhaccuatui.Models;
+
 
 namespace nhaccuatui.Controllers
 {
@@ -13,5 +16,28 @@ namespace nhaccuatui.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult XulyDangNhap(string username, string password)
+        {
+            NhaccuatuiModel db = new NhaccuatuiModel();
+
+            // Modify the query to match username and password (hashing recommended for security)
+            var query = $"SELECT * FROM Users WHERE Username = '{username}' AND PasswordHash = '{password}'";
+            var result = db.get(query);
+
+            if (result.Count > 0)
+            {
+                // Store user data in session on successful login
+                Session["taikhoan"] = result[0];
+                return RedirectToAction("Index", "Home"); // Redirect to the desired page on success
+            }
+            else
+            {
+                TempData["LoginError"] = "Sai tên đăng nhập hoặc mật khẩu"; // Set error message on failure
+                return RedirectToAction("Index", "LoginRegister"); // Redirect back to login page
+            }
+        }
+
+
     }
 }
